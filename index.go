a85b89ec28ec837b4d581bce9870d7d60d894b31
bjmashele <bjmashele@gmail.com>
2019-07-06 17:19:50 -0500

    Added a third party router (mux)

--- index.go
+++ index.go
@@ -5,12 +5,17 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", index)
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
