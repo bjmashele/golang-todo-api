06c9ec75324cb166e2ac9cbbad909f7c165c8f73
bjmashele <bjmashele@gmail.com>
2019-07-09 18:11:57 -0500

    Added a CreateTodo route

--- Todo.go
+++ Todo.go
@@ -3,7 +3,7 @@ package main
 import "time"
 
 type Todo struct {
-	id        int       `json:"id"`
+	Id        int       `json:"id"`
 	Name      string    `json:"name"`
 	completed bool      `json:"completed"`
 	Due       time.Time `json:"due"`
